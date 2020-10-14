529c5152692229fcc05a2868e66cbb71b0fc907c
carl.liu <carl.liu@comcast.com>
2020-10-13 18:28:04 -0700

    initial commit

---
+++ src/main/java/com/demo/kafka/controllers/KafkaController.java
@@ -0,0 +1,32 @@
+package com.demo.kafka.controllers;
+
+import com.demo.kafka.engine.Producer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/kafka")
+public class KafkaController {
+
+    private final Logger logger = LoggerFactory.getLogger(KafkaController.class);
+
+    private final Producer producer;
+
+    @Autowired
+    KafkaController(Producer producer) {
+        this.producer = producer;
+    }
+
+    @PostMapping(value = "/publish")
+    public void sendMessageToKafkaTopic(@RequestParam("message") String message) {
+        logger.info("sendMessageToKafkaTopic, message:= [{}]", message);
+        this.producer.sendMessage(message);
+    }
+
+
+}
